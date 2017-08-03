93864c6f5e04c83000e096994fce764f32dcdcec
Didymus <didymus.benson@gmail.com>
2017-08-03 10:50:10 -0600

    fixed async stuff

--- check.js
+++ check.js
@@ -1,12 +1,15 @@
-var fs = require('fs');
+var fs = require('graceful-fs');
 var _ = require('lodash');
+var async = require('async');
 var dsv = require('d3-dsv');
 var path = "./courses";
 var dirs = getdirs(path);
-var curpath;
-var hits = [];
-console.log(dirs);
 
+//Reset the hits output file.
+fs.writeFile('./hits.csv', '', (err) => {
+    if (err) throw err;
+    console.log('Hits file emptied.')
+})
 
 // Grab the directories in ./courses
 function getdirs(path) {
@@ -19,32 +22,47 @@ function getdirs(path) {
 function gethits(dirs) {
     // Keep track of async task and write file when loop is done
     var dirdone = _.after(dirs.length, loghits)
-    dirs.forEach(function (dir) {
-        curpath = path + '/' + dir;
+    async.eachOf(dirs, function (value, key, callback) {
+
+        var curpath = path + '/' + value;
+
         fs.readdir(curpath, function (err, files) {
             if (err) throw err;
             // Keep track of async task and trigger when done
             var filesdone = _.after(files.length, dirdone)
+
             files.forEach(function (file, index) {
-                fs.readFile(curpath + '/' + file, 'utf8', (err, data) => {
-                    if (err) throw err;
-                    // SEARCH STRING HERE.
-                    if (data.indexOf('&ltscript') >= 0) {
-                        //console.log('[' + curpath + '/' + file + '] ' + ' [' + data.indexOf('%ltscript') + ']');
-                        hits.push(curpath + '/' + file);
-                    }
-                    // Tell lodash that this file has been read.
-                    filesdone();
-                })
+                var filepath = curpath + '/' + file
+                if (!(fs.statSync(filepath).isDirectory())) {
+
+                    fs.readFile(filepath, 'utf8', (err, data) => {
+                        if (err) throw err;
+                        // SEARCH STRING HERE.
+                        if (data.indexOf('butt') >= 0) {
+                            fs.appendFile('./hits.csv', filepath + ', \r\n', (err) => {
+                                if (err) throw err;
+                                console.log(filepath + " added to hits.");
+                            });
+                        }
+                        // Tell lodash that this file has been read.
+                        filesdone();
+                    })
+                }
             })
         })
+    }, function (err) {
+        if (err) throw err;
     })
 };
 
 // Write output to file
 function loghits() {
+    console.log("loghits");
     // Write hits to CSV or other output file
-    console.log(hits);
+    fs.writefile('./hits.csv', hits, (err) => {
+        if (err) throw err;
+        console.log('Done.')
+    })
 }
 
 gethits(dirs);
